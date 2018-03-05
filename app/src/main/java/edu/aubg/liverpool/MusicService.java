98cc7cbbb7446cd1523b08e5c733a728cafd6e43
Radoslav Genov <radoslavgenov5@hotmail.com>
2018-03-05 13:16:57 +0200

    first

---
+++ app/src/main/java/edu/aubg/liverpool/MusicService.java
@@ -0,0 +1,32 @@
+package edu.aubg.liverpool;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MusicService extends Service {
+    MediaPlayer mPlayer;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        mPlayer = MediaPlayer.create(this, R.raw.piano);
+        mPlayer.start();
+        if (mPlayer != null){
+            mPlayer.setLooping(true);
+            mPlayer.setVolume(100, 100);
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
+}
